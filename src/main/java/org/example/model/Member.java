eb1a3d961a4b7bf5210b1005ef384870c3654eca
Kewon <yang0184@naver.com>
2023-11-06 00:30:02 +0900

    fix: Column Mapping

--- src/main/java/org/example/model/Member.java
+++ src/main/java/org/example/model/Member.java
@@ -1,36 +1,50 @@
 package org.example.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Random;
 
 
 // JPA 내부적으로 리플렉션을 쓰기때문에 기본생성자가 있어야한다.(public일 필요는 없음)
 @Entity
+@Table(name="member")
 @Getter @Setter @NoArgsConstructor
 @AllArgsConstructor @ToString
 @Builder
 public class Member {
     @Id // 최소한 Key가 무엇인지 알려줘야한다.
     private Long id;
+
     private String name;
-    private LocalDateTime time;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdDate;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime lastModifiedDate;
+
+    @Lob
+    private String description;
+
 
     public static Member random() {
         long id = (long) ( Math.random() * 100000 );
         String name = createName();
 
-        System.out.println("id = " + id);
-        System.out.println("name = " + name);
-
         return Member.builder()
                 .id(id)
                 .name(name)
-                .time(LocalDateTime.now())
+                .createdDate(LocalDateTime.now())
+                .age(50)
+                .roleType(RoleType.USER)
+                .description(String.format("%d / %s", id, name))
                 .build();
     }
 
---
+++ src/main/java/org/example/model/RoleType.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+public enum RoleType {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    RoleType(String user) {}
+}
